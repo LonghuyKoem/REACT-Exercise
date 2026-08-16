dd57d45ad2db16235b15007b409fe04f29e1642f
Longhuy Koem <longhuykoem@gmail.com>
2026-08-16 23:40:43 +0700

    Update

--- REACT-Exercise/src/components/layers/FooterComponent.tsx
+++ REACT-Exercise/src/components/layers/FooterComponent.tsx
@@ -1,41 +1,30 @@
 export function FooterComponent() {
   return (
-    <div className="min-h-screen px-4 py-3">
-      <div className="mx-auto max-w-[11000px] rounded-lg border border-gray-200 bg-white shadow-sm">
-        <header className="flex items-center justify-between px-12 py-9">
-          <div className="flex items-center gap-4">
-            <div className="flex h-7 w-7 items-center justify-center">
-              <span className="text-[10px] font-bold leading-[9px] text-gray-700">
-                CAN
-                <br />
-                CRAZE
-              </span>
-            </div>
+    <header className="flex items-center justify-between px-5 py-7 sm:px-12 sm:py-9">
+      <div className="flex items-center gap-4">
+        <img
+          src="https://imgs.search.brave.com/mt-HWrWKQUMDnGn3ZJDvkHsaf8Zck5jaFn5kGZ6Ycts/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9pLm5h/dGdlb2ZlLmNvbS9u/LzMzM2QyYjYxLWEz/MDktNDc4My05YTRj/LWY2ZGQ1M2U4YjBk/ZS8xMDkwLmpwZw"
+          alt="Galaxy Store logo"
+          className="h-12 w-12 object-cover"
+        />
 
-            <span className="text-lg font-semibold text-gray-900">
-              Can Craze
-            </span>
-          </div>
-
-          <nav className="flex items-center gap-6">
-            <a className="text-xs font-medium text-gray-500 transition hover:text-gray-900">
-              About
-            </a>
-
-            <a className="text-xs font-medium text-gray-500 transition hover:text-gray-900">
-              Contact
-            </a>
-          </nav>
-        </header>
-
-        <div className="mx-12 border-t border-gray-200"></div>
-
-        <footer className="px-12 py-7 text-center">
-          <p className="text-xs text-gray-500">
-            © 2025 Can Craze™. All Rights Reserved.
-          </p>
-        </footer>
+        <span className="text-lg font-semibold text-gray-900">
+          Galaxy Store
+        </span>
       </div>
-    </div>
+
+      <nav className="flex items-center gap-6">
+        <a className="text-xs font-medium text-gray-500 transition hover:text-gray-900">
+          About
+        </a>
+
+        <a
+          href="#"
+          className="text-xs font-medium text-gray-500 transition hover:text-gray-900"
+        >
+          Contact
+        </a>
+      </nav>
+    </header>
   );
 }
